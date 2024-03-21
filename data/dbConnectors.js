2584243a64a3f5953a0c87b1a49b835eb50a5cce
Jay Gurnani <gurnanijay1999@gmail.com>
2024-03-21 08:38:21 +0530

    graphql

---
+++ data/dbConnectors.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { Sequelize, DataTypes } from "sequelize";
+import _ from 'lodash';
+import casual from "casual"
+
+async function connectMongo() {
+    try {
+        await mongoose.connect('mongodb://localhost/widgets');
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log('Error connecting to MongoDB:', error);
+    }
+}
+connectMongo();
+
+const widgetSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    soldout: String,
+    inventory: String,
+    stores: Array,
+});
+
+const Widgets = mongoose.model('widgets', widgetSchema)
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+const Categories = sequelize.define('categories', {
+    category: DataTypes.STRING,
+    description: DataTypes.STRING,
+});
+
+async function syncAmdSeedCategories() {
+    try {
+        await sequelize.sync( { force: true});
+        console.log('SQLITE connection established and Categories model synced')
+        
+        // Seed Categories
+        await Promise.all(_.times(5, () => {
+            return Categories.create({
+                category: casual.word,
+                description: casual.sentence,
+            })
+        }))
+
+        console.log('Categories Seeded')
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+syncAmdSeedCategories();
+export { Widgets , Categories};
